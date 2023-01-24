500f6769e0bdbf077dedd628b84af979e75b74ad
Mihai-Alin Sendre <mihai.alin.sendre@gmail.com>
2023-01-24 01:05:15 +0000

    Added aliases

--- A Tour of C++/Templates/parameterized_type.cpp
+++ A Tour of C++/Templates/parameterized_type.cpp
@@ -1,14 +1,16 @@
 
 #include <stdexcept>
+#include <iostream>
 
 template<typename T>
 class Vector {
-
 private:
   T* elem;
   int sz;
 
 public:
+  using value_type = T;
+
   Vector(int s);
   ~Vector() { delete[] elem; }
 
@@ -70,3 +72,50 @@ const T* end(const Vector<T>& v)
   return (v.size() > 0) ? (&v[0] + v.size()) : nullptr;
 }
 
+// Alias
+using IntVector = Vector<int>;
+
+// As generic a function as possible
+template<typename Container>
+typename Container::value_type mySum(const Container& c)
+{
+  typename Container::value_type acc = 0;
+  for (const typename Container::value_type& v : c)
+    acc += v;
+
+  return acc;
+}
+
+// Alias v2
+template<typename T>
+using ValueType = typename T::value_type;
+
+template<typename Container>
+ValueType<Container> newSum(const Container& c)
+{
+  ValueType<Container> acc = 0;
+
+  for (const ValueType<Container>& v : c)
+    acc += v;
+
+  return acc;
+}
+//
+
+int main()
+{
+  IntVector v(5);
+  v[0] = 1;
+  v[1] = 3;
+  v[2] = 0;
+  v[3] = 1;
+  v[4] = 5;
+
+  int sum = mySum(v);
+  std::cout << "sum = " << sum << std::endl;
+
+  int sum2 = newSum(v);
+  std::cout << "sum2 = " << sum2 << std::endl;
+
+  return 0;
+}
